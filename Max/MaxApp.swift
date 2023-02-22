5f8e4582fc1ecaeac7b9e69cd9586dfac1c5138b
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-02-22 20:19:47 +0530

    Initial Commit

---
+++ Max/MaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxApp.swift
+//  Max
+//
+//  Created by avataar on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
